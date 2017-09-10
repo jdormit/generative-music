63a4b416eb9aa2d9918ccd268b690c8f081e5aeb
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-09-09 22:26:51 -0400

    Implement an oscillator

--- build/bundle.js
+++ build/bundle.js
@@ -1,1 +1,6 @@
-var audioContext = new window.AudioContext();
+var audioContext = new AudioContext();
+var gainNode = audioContext.createGain();
+gainNode.connect(audioContext.destination);
+var osc = audioContext.createOscillator();
+osc.connect(gainNode);
+osc.start();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-let audioContext : AudioContext = new window.AudioContext();
+const audioContext = new AudioContext();
+const gainNode = audioContext.createGain();
+
+gainNode.connect(audioContext.destination);
+
+const osc = audioContext.createOscillator();
+osc.connect(gainNode);
+osc.start();
